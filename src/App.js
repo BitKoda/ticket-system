988ee51abb3d6f961434a8ce533cae6d8f37b52c
Jamie Finlay <jpfinlay@gmail.com>
2022-02-24 15:36:18 +0000

    Form fields created, and input controlled by React

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
 import './App.css';
+import {useState} from "react";
+
 
 function App() {
+  const [form, setForm] = useState({username: "", problem: "", zoomUrl: ""})
+  const handleChange = (e) => {
+    const name = e.target.name;
+    console.log(name)
+    const value = e.target.value;
+    setForm(values => ({...values, [name]: value }))
+    console.log(form)
+  }
+
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>NC Help</h1>
+      <form> 
+        <label htmlFor="username">Username</label>
+        <input type="text" id="username" value={form.username} name="username" onChange={handleChange} />
+
+        <label htmlFor="problem">How can we help?</label>
+        <textarea id="problem" value={form.problem} name="problem" onChange={handleChange}></textarea>
+        
+        <label htmlFor="zoomUrl">Zoom Link</label>
+        <input type="text" id="zoomUrl" value={form.zoomUrl} name="zoomUrl" onChange={handleChange} />
+        
+        <button id="submit">Help!</button>
+      </form>
     </div>
   );
 }
