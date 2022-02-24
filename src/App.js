d00b93ca4a051a87e705e223ab5b9ff4938d7fa9
Adam <aw@little.agency>
2022-02-24 16:40:49 +0000

    added submit buttton functionality and setup static help history log

--- src/App.js
+++ src/App.js
@@ -1,32 +1,59 @@
 import './App.css';
 import {useState} from "react";
-
+import ReactDOM from 'react-dom';
 
 function App() {
   const [form, setForm] = useState({username: "", problem: "", zoomUrl: ""})
   const handleChange = (e) => {
     const name = e.target.name;
-    console.log(name)
     const value = e.target.value;
     setForm(values => ({...values, [name]: value }))
-    console.log(form)
+  }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    HistoryLog();
+  }
+  const HistoryLog = () => {
+    const helpLog = 
+    <div>
+      {Object.entries(form).map(([key,val]) => 
+        <p key={key}> 
+          {key}:{val}
+        </p>
+      )}
+    </div>
+    ReactDOM.render(helpLog, document.getElementById('helpLogContainer'));
   }
 
   return (
     <div>
-      <h1>NC Help</h1>
-      <form> 
-        <label htmlFor="username">Username</label>
-        <input type="text" id="username" value={form.username} name="username" onChange={handleChange} />
+      <section id="form-section"> 
+        <h1>NC Help</h1>
+        <form onSubmit={handleSubmit}> 
+          <div class="form-group">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" value={form.username} name="username" onChange={handleChange} />
+          </div>
+          <div class="form-group">
+            <label htmlFor="problem">How can we help?</label>
+            <textarea id="problem" value={form.problem} name="problem" onChange={handleChange}></textarea>
+          </div>
+          <div class="form-group">
+            <label htmlFor="zoomUrl">Zoom Link</label>
+            <input type="text" id="zoomUrl" value={form.zoomUrl} name="zoomUrl" onChange={handleChange} />
+          </div>
+          <div class="form-group">
+            <input type="submit" id="submit" value="Help me Helpdesk!"/>
+          </div>
+        </form>
+      </section>
+      <hr></hr>
+      <h2>
+        NC Help History Log
+      </h2>
+      <div id="helpLogContainer">
 
-        <label htmlFor="problem">How can we help?</label>
-        <textarea id="problem" value={form.problem} name="problem" onChange={handleChange}></textarea>
-        
-        <label htmlFor="zoomUrl">Zoom Link</label>
-        <input type="text" id="zoomUrl" value={form.zoomUrl} name="zoomUrl" onChange={handleChange} />
-        
-        <button id="submit">Help!</button>
-      </form>
+      </div>
     </div>
   );
 }
