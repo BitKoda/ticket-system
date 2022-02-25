605ccc79b6aedbbbb5b820b53219cc3efcdf3640
Jamie Finlay <jpfinlay@gmail.com>
2022-02-25 16:32:47 +0000

    Minor CSS changes

--- src/components/TicketItem.jsx
+++ src/components/TicketItem.jsx
@@ -1,18 +1,20 @@
 export default function TicketItem({ticket, updateResolved, deleteResolvedTicket}) {
     return (
         <li className="single-ticket-list">
-            <h3> Hello, {ticket.username} </h3>
-            <p> {ticket.problem} </p>
-            <a href={ticket.zoomUrl} > Click here to join the zoom </a>
-            <p> Your ticket is being assigned to: {ticket.tutor}</p>
-            <hr></hr>
-            <button className={ticket.resolved ? "resolved" : "open"} onClick={() => updateResolved(ticket)}>
-                {ticket.resolved ? "Resolved" : "Open"}
-            </button>
-            <button className={ticket.resolved ? "display" : "hide"} 
-                    onClick={() => deleteResolvedTicket(ticket)}>
-                Delete
-            </button>
+            <div>
+                <h3> Hello, {ticket.username} </h3>
+                <p> {ticket.problem} </p>
+                <a href={ticket.zoomUrl} > Click here to join the zoom </a>
+                <p> Your ticket is being assigned to: {ticket.tutor}</p>
+                <hr></hr>
+                <button className={ticket.resolved ? "resolved" : "open"} onClick={() => updateResolved(ticket)}>
+                    {ticket.resolved ? "Resolved" : "Open"}
+                </button>
+                <button className={ticket.resolved ? "display" : "hide"} 
+                        onClick={() => deleteResolvedTicket(ticket)}>
+                    Delete
+                </button>
+            </div>
         </li>
 
     )
